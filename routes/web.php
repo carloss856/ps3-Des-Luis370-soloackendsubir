93e1b8b15967e7fb5a7ac54d8ba58f503f1ab6dd
carloss856 <carlos.lamole98@gmail.com>
2026-06-18 15:26:32 -0400

    Fix routes/web.php vacio (causaba headers already sent)

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,11 @@
+<?php
 
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return response()->json([
+        'app' => 'Luis370 API',
+        'status' => 'ok',
+        'docs' => 'usa /api/* para los endpoints',
+    ]);
+});

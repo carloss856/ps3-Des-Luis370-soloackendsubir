c18a1f4fc56e38de1909f2967b79531b01c29673
carloss856 <carlos.lamole98@gmail.com>
2026-06-18 16:25:34 -0400

    Ruta /diag temporal: chequear TLS del driver mongodb

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,22 @@ Route::get('/', function () {
         'docs' => 'usa /api/* para los endpoints',
     ]);
 });
+
+// Diagnóstico temporal: ¿el driver mongodb tiene TLS/SSL? Borrar después.
+Route::get('/diag', function () {
+    ob_start();
+    phpinfo(INFO_MODULES);
+    $info = ob_get_clean();
+    $tls = 'no encontrado';
+    foreach (['libmongoc SSL', 'libmongoc crypto', 'SSL library version', 'crypto library'] as $needle) {
+        if (preg_match('/' . preg_quote($needle, '/') . '.*$/mi', $info, $m)) {
+            $tls = trim($m[0]);
+            break;
+        }
+    }
+    return response()->json([
+        'mongodb_ext' => phpversion('mongodb'),
+        'tls_line' => $tls,
+        'openssl_ext' => extension_loaded('openssl'),
+    ]);
+});

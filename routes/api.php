aa078537e85c45ae0735eeb2f134599233433f62
carloss856 <carlos.lamole98@gmail.com>
2026-06-18 16:33:02 -0400

    Mover diag a /api/diag (rutas web fallan por sesion)

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,22 @@ use App\Http\Controllers\StatsController;
 use App\Http\Controllers\PermissionsController;
 use App\Http\Controllers\RbacController;
 
+// Diagnóstico temporal: TLS del driver mongodb. Borrar después.
+Route::get('/diag', function () {
+    ob_start();
+    phpinfo(INFO_MODULES);
+    $info = ob_get_clean();
+    $tls = 'no encontrado';
+    foreach (['libmongoc SSL', 'libmongoc crypto', 'SSL library version', 'crypto library'] as $needle) {
+        if (preg_match('/' . preg_quote($needle, '/') . '.*$/mi', $info, $m)) { $tls = trim($m[0]); break; }
+    }
+    return response()->json([
+        'mongodb_ext' => phpversion('mongodb'),
+        'tls_line' => $tls,
+        'openssl_ext' => extension_loaded('openssl'),
+    ]);
+});
+
 // Rutas públicas
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
